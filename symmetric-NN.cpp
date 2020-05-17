db7aed30c6e604745888fefeb34687e7e793dd67
Cybernetic1 <generic.intelligence@gmail.com>
2020-05-17 16:38:04 +0800

    toilet

--- symmetric-NN.cpp
+++ symmetric-NN.cpp
@@ -6,7 +6,6 @@
 // The g-layers form a regular NN.
 // The h-layers form another NN but it is averaged over m inputs, where m is the multiplicity
 
-
 #include <iostream>
 #include <cstdio>
 #include <random>
@@ -60,78 +59,6 @@ double random01()
 	return rand() * 2.0 / (float) RAND_MAX - 1.0;
 	}
 
-// In the current interpretation, each point is a set
-// We want to measure the distance between 2 points as sets and also as lists
-// The distance between 2 lists, where each "coordinate" belongs to one dimension, is the
-// "standard" Euclidean distance:
-//      d(x,y) = sqrt((x1 - y1)^2 + (x2 - y2)^2)
-double distance_Eu(double x[], double y[])
-	{
-	double sum = 0.0;
-
-	for (int i = 0; i < N; ++i)
-		sum += pow(x[i] - y[i], 2);
-
-	return sqrt(sum);
-	}
-
-// The set distance must satisfy 2 requirements simultaneously:
-// 1) The distance should be 0 under permutations
-// 2) The distance attains its maximum when 2 points are most dissimilar, and would equal the
-//		Euclidean distance between them.
-double set_distance(double x[], double y[])
-	{
-	double sum, sum1, sum2 = 0.0;
-
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
-			sum += pow(x[i] - y[j], 2);
-
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
-			sum1 += pow(x[i] - x[j], 2);
-
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
-			sum2 += pow(y[i] - y[j], 2);
-
-	return (2 * sqrt(sum / N) - sqrt(sum1 / N) - sqrt(sum2 / N)) / 2;
-	}
-
-// ***** This miraculously good-looking function was found by serendipity
-// Here the inputs x and y are the Euclidean and "set" distances
-double joint_penalty(double x, double y)
-	{
-	double k = 30.0			// "Steepness"
-
-	return exp(-k * (x * x + y * y)) - exp(-2.0 * k * x * y);
-	}
-
-// Randomly permute x, store the result in y
-// Or randomly generate a new point y
-// Returns whether a permutation has occurred
-bool perturb(double x[], double y[])
-	{
-	if (random01() > 0.5)
-		{
-		// Copy x to y
-		for (int i = 0; i < N; ++i)
-			y[i] = x[i];
-		// Apply random permutation
-		std::random_shuffle(y, y + N);
-		return true;
-		}
-	else
-		{
-		// generate new random value
-		for (int i = 0; i < N; ++i)
-			y[i] = random01() * 2.0 - 1.0;
-		return false;
-		}
-	}
-
-// Learn the "neuromancer" map, ie, a vector-to-vector map that is permutation invariant.
-
 // To test convergence, we record the sum of squared errors for the last M and last M..2M
 // trials, then compare their ratio.
 
@@ -144,8 +71,10 @@ int main(int argc, char **argv)
 	{
 	int neuronsPerLayer[] = {N, 10, 10, 8, N}; // first = input layer, last = output layer
 	int numLayers = sizeof (neuronsPerLayer) / sizeof (int);
-	NNET *Net = create_NN(numLayers, neuronsPerLayer);
-	LAYER lastLayer = Net->layers[numLayers - 1];
+	NNET *Net_g = create_NN(numLayers, neuronsPerLayer);
+	NNET *Net_h = create_NN(numLayers, neuronsPerLayer);
+	LAYER lastLayer_g = Net_g->layers[numLayers - 1];
+	LAYER lastLayer_h = Net_h->layers[numLayers - 1];
 
 	int userKey = 0;
 	#define M	50			// how many errors to record for averaging
@@ -228,12 +157,8 @@ int main(int argc, char **argv)
 		// The neural network maps X1 to Y1, and X2 to Y2, independently.
 		//		errors1 = errors for Y1 (dim N)
 		//		errors2 = errors for Y2 (dim N)
-		bool permuted = perturb(X1, X2);			// result stored in X2
-		double d1 = set_distance(K, K2);
-		ForwardPropMethod(Net, N, X1);			// X1 is now Y1
-		ForwardPropMethod(Net, N, X2);			// X2 is now Y2
-		double d2 = distance_Eu(K, K2);
-		double penalty = joint_penalty(d1, d2);
+		ForwardPropMethod(Net_g, N, X1);			// X1 is now Y1
+		ForwardPropMethod(Net_h, N, X2);			// X2 is now Y2
 
 		training_err += fabs(error); // record sum of errors
 		// printf("sum of squared error = %lf  ", training_err);
@@ -259,20 +184,6 @@ int main(int argc, char **argv)
 		if (tail == M) // loop back in cycle
 			tail = 0;
 
-		// With only the "joint penalty", we cannot evaluate error of individual components,
-		//		thus traditional back-prop is not applicable.
-		// The array errors[N] contains errors for each component with respect to an ideal output.
-		// 
-
-		if (permuted)
-			{
-			// Error is the difference between the output points Y1 and Y2
-			
-			}
-		else
-			{
-			// Error is calculated from 
-			}
 		// plot_W(Net);
 		back_prop(Net, errors);
 		// plot_W(Net);
@@ -290,7 +201,7 @@ int main(int argc, char **argv)
 					K[k] = ((double) rand() / (double) RAND_MAX);
 				// plot_tester(K[0], K[1]);
 
-				ForwardPropMethod(Net, 2, K);
+				ForwardPropMethod(Net_g, 2, K);
 
 				// Desired value = K_star
 				double single_err = 0.0;
@@ -387,66 +298,6 @@ int main(int argc, char **argv)
 	//	pause_graphics();
 	// else
 	//	quit_graphics();
-	free_NN(Net, neuronsPerLayer);
-	}
-
-// Test forward propagation
-
-#define ForwardPropMethod	forward_prop_sigmoid
-void forward_test()
-	{
-	int neuronsPerLayer[4] = {4, 3, 3, 2}; // first = input layer, last = output layer
-	int numLayers = sizeof (neuronsPerLayer) / sizeof (int);
-	NNET *Net = create_NN(numLayers, neuronsPerLayer);
-	LAYER lastLayer = Net->layers[numLayers - 1];
-	double sum_error2;
-
-	// start_NN_plot();
-	// start_W_plot();
-	// start_K_plot();
-
-	printf("This test sets all the weights to 1, then compares the output with\n");
-	printf("the test's own calculation, with 100 randomized inputs.\n\n");
-
-	// Set all weights to 1
-	for (int l = 1; l < numLayers; l++) // for each layer
-		for (int n = 0; n < neuronsPerLayer[l]; n++) // for each neuron
-			for (int k = 0; k <= neuronsPerLayer[l - 1]; k++) // for each weight
-				Net->layers[l].neurons[n].weights[k] = 1.0f;
-
-	for (int i = 0; i < 100; ++i)
-		{
-		// Randomize K
-		double sum = 1.0f;
-		for (int k = 0; k < 4; ++k)
-			{
-			K[k] = (rand() / (float) RAND_MAX) * 2.0 - 1.0;
-			sum += K[k];
-			}
-
-		ForwardPropMethod(Net, 4, K);
-
-		// Expected output value:
-		//double K_star = (2.0f * (3.0f * sigmoid(3.0f * sigmoid(4.0f * sigmoid(sum) + 1.0f) + 1.0f) + 1.0f) + 1.0f);
-		double K_star = 2.0f * sigmoid(3.0f * sigmoid(3.0f * sigmoid(sum + 1.0f) + 1.0f) + 1.0f) + 1.0f;
-		// Calculate error
-		sum_error2 = 0.0f;
-		for (int k = 0; k < 2; ++k)
-			{
-			// Difference between actual outcome and desired value:
-			double error = lastLayer.neurons[k].output - K_star;
-			sum_error2 += (error * error); // record sum of squared errors
-			}
-
-		// plot_W(Net);
-		// plot_NN(Net);
-		// plot_trainer(0);
-		// plot_K();
-		// delay_vis(50);
-
-		printf("iteration: %05d, error: %lf\n", i, sum_error2);
-		}
-
-	// pause_graphics();
-	free_NN(Net, neuronsPerLayer);
+	free_NN(Net_g, neuronsPerLayer);
+	free_NN(Net_h, neuronsPerLayer);
 	}
